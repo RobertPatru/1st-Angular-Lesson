6fb57c41193644297706575a4fd15777f6798328
RobertPatru <86546157+RobertPatru@users.noreply.github.com>
2022-08-16 18:49:19 +0300

    w

--- angular-app/src/app/components/user/user.component.ts
+++ angular-app/src/app/components/user/user.component.ts
@@ -7,5 +7,18 @@ import { Component } from "@angular/core";
 })
 
 export class UserComponent {
+    // Properties
+    firstName = "Robert";
+    lastName = "Baratheon";
+    age = 34;
 
-}
+    // Methods
+    constructor () {
+        console.log('This is the constructor inside the user component');
+        this.saySomething();
+    }
+
+    saySomething () {
+        console.log(`First Name: ${this.firstName}`);
+    }
+} 
