622df2ce7db5d62b6a1f0a051e0ffcf4b3a463a4
RobertPatru <86546157+RobertPatru@users.noreply.github.com>
2022-08-19 17:44:44 +0300

    ngIf

--- angular-app/src/app/components/users/users.component.ts
+++ angular-app/src/app/components/users/users.component.ts
@@ -9,61 +9,68 @@ import { User } from 'src/app/models/User';
 
 export class UsersComponent implements OnInit {
   users: any;
+  showExtended: boolean = true;
+  loading: boolean = true;
 
   constructor() {
 
   }
 
   ngOnInit(): void {
-    this.users = [
-      {
-        firstName: 'Robert',
-        lastName: 'Baratheon',
-        age: 34,
-        address: {
-          street: 'Linia Mare',
-          city: 'Ciocanesti',
-          number: 11
+    setTimeout(() => {
+      this.users = [
+        {
+          firstName: 'Robert',
+          lastName: 'Baratheon',
+          age: 34,
+          address: {
+            street: 'Linia Mare',
+            city: 'Ciocanesti',
+            number: 11
+          }
+        },
+        {
+          firstName: 'Kevin',
+          lastName: 'Burche',
+          age: 124,
+          address: {
+            street: 'a Mare',
+            city: 'dsa',
+            number: 112
+          }
+        },
+        {
+          firstName: 'adwa',
+          lastName: 'dsa',
+          age: 324,
+          address: {
+            street: 'Linia dsa',
+            city: 'sda',
+            number: 121
+          }
         }
-      },
-      {
-        firstName: 'Kevin',
-        lastName: 'Burche',
-        age: 124,
-        address: {
-          street: 'a Mare',
-          city: 'dsa',
-          number: 112
-        }
-      },
-      {
-        firstName: 'adwa',
-        lastName: 'dsa',
-        age: 324,
-        address: {
-          street: 'Linia dsa',
-          city: 'sda',
-          number: 121
-        }
-      }
-    ];
+      ];
 
-    this.addUser(
-      {
-        firstName: 'david ceva',
-        lastName: 'aoleu',
-        age: 324,
-        address: {
-          street: 'aoleu aoleu',
-          city: 'aoleu',
-          number: 121
-        }
-      }
-    )
+      this.loading = false;
+    }, 2000);
+    
+
+    // this.addUser(
+    //   {
+    //     firstName: 'david ceva',
+    //     lastName: 'aoleu',
+    //     age: 324,
+    //     address: {
+    //       street: 'aoleu aoleu',
+    //       city: 'aoleu',
+    //       number: 121
+    //     }
+    //   }
+    // );
   }
 
-  addUser (user: User) {
-    this.users.push(user);
-  }
+  // addUser (user: User) {
+  //   this.users.push(user);
+  // }
 
 }
